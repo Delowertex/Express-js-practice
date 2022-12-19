7a83b113608ce2f37b672844659dbdf449a335a1
Delower <coderdelower@gmail.com>
2022-12-20 02:43:33 +0600

    response json-redirect-download

--- practiceAppOne/index.js
+++ practiceAppOne/index.js
@@ -4,8 +4,54 @@ app = express();
 
 app.get("/", function(req, res){
     res.send("Hellow express js");
-})
+});
+app.post("/about", function(req, res){
+    res.send("Hellow about");
+});
+app.put("/contact", function(req, res){
+    res.send("Hellow contact");
+});
+app.delete("/terms", function(req, res){
+    res.send("Hellow terms");
+});
+
+//simple response
+app.get("/one", function(req, res){
+    res.send("simple string respond");
+});
+
+//status code 
+app.post("/two", function(req, res){
+    res.status(555).end("response status code");
+});
+
+//json response
+app.get("/three", function(req, res){
+    var jesonData = [
+        {
+            "name":  "Delower",
+            "age": "36",
+            "class" : "ten"
+        }
+    ]
+    res.send(jesonData);
+});
+
+//response downlaod
+app.post("/four", function(req, res){
+    res.download("./image/images.jpg");
+});
+
+//response redirect
+app.post("/bangladesh", function(req, res){
+    res.redirect("http://localhost:8000/india");
+});
+
+app.post("/india", function(req, res){
+    res.send("This is india");
+});
+
 
 app.listen(8000, function(){
     console.log("server run success");
-})
+});
